3712365762a36f7e20f08949143c75e241c77668
jegawaran <jagatheesjothi@gmail.com>
2021-01-02 23:41:33 +0530

    apps added core and store, core app for frontend tech(vue) and store for backend

--- yennavenum/yennavenum/urls.py
+++ yennavenum/yennavenum/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.core import views
 
 urlpatterns = [
+    path('',views.frontpage, name='frontpage'),
+    path('contact',views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
